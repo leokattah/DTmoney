511190d26d4d74e28d242ea5684d0affe923ccae
leokattah <leokattah@gmail.com>
2021-07-06 21:43:16 -0300

    components in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/globals';
+import {Dashboard} from './components/Dashboard'
 
 const Title = styled.h1`
 color: #8257e6;
@@ -10,6 +11,7 @@ export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
