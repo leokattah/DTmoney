fd2c29fd297f2fefee1308dd0045e443a062b837
leokattah <leokattah@gmail.com>
2021-07-07 21:47:44 -0300

    routes done with miragejs and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import styled from 'styled-components'
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/globals';
 import {Dashboard} from './components/Dashboard'
 
-const Title = styled.h1`
-color: #8257e6;
-`
+
 
 export function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import {createServer} from 'miragejs';
 
+createServer({
+  routes(){
+    this.namespace = 'api'; // 'api' é a pasta que foi definida por mim no arquivo index.tsx na rota de fetch.
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'transaction 1',
+          amout: 400,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
